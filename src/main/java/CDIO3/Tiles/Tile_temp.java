9c1fa5bda18f7a9f8fa35eb64fd8b28acc42b64a
dendersen <dendersen@endersen.dk>
2023-11-08 09:51:28 +0100

    two temporary classes to handle minimum requirements for Board and tile in TUI

---
+++ src/main/java/CDIO3/Tiles/Tile_temp.java
@@ -0,0 +1,12 @@
+package CDIO3.Tiles;
+
+/**
+ * Tile_debug
+ */
+public class Tile_temp extends Tile{
+
+  public Tile_temp(Color color, String name, int number) {
+    super(color, name, number);
+  }
+
+}
