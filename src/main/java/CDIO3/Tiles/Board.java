b03837401d197bdfdbd7527b1e946f26e68af64f
Daniel <danihansen74@gmail.com>
2023-11-14 16:02:39 +0100

    added a method to board class

--- src/main/java/CDIO3/Tiles/Board.java
+++ src/main/java/CDIO3/Tiles/Board.java
@@ -1,5 +1,11 @@
 package CDIO3.Tiles;
 
 public class Board {
-    
+    private Tile[] tiles = new Tile[24];
+public Board(){
+
+}
+  public Tile getTile(int index){
+    return tiles[index];
+  }
 }
