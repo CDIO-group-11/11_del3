5580546d59a864ec3c61c77b0762c9b1733de8d3
Zling <Mattias@bresdahl.dk>
2023-11-14 16:08:06 +0100

    Lavede relevante fields, Wallet, Piece og PropertyDeeds, samt lavede getter til alle tre

--- src/main/java/CDIO3/Player/Player.java
+++ src/main/java/CDIO3/Player/Player.java
@@ -3,4 +3,18 @@ package CDIO3.Player;
 public class Player {
     Wallet WalletObj1 = new Wallet();
     public static int Pamount;
+    int Wallet;
+    int Piece;
+    String PropertyDeeds;
+
+    public int getWallet() {
+        return Wallet;
+    }
+    public int getPiece() {
+        return Piece;
+    }
+    public String getPropertyDeeds() {
+        return PropertyDeeds;
+    }
+    
 }
