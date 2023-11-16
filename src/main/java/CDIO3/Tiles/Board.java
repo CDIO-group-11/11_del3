fe05768afe0eb16e764ca101d33865cc2f7f7db2
dendersen <dendersen@endersen.dk>
2023-11-16 16:42:11 +0100

    added support for missing tile handling

--- src/main/java/CDIO3/Tiles/Board.java
+++ src/main/java/CDIO3/Tiles/Board.java
@@ -1,10 +1,10 @@
 package CDIO3.Tiles;
 
 public class Board {
-    private Tile[] tiles = new Tile[24];
-public Board(){
-
-}
+  private Tile[] tiles = new Tile[24];
+  public Board(){
+    
+  }
   public Tile getTile(int index){
     return tiles[index];
   }
