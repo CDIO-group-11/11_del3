69b9c29254c24f1a52971e80cbe88e603be12a0c
dendersen <dendersen@endersen.dk>
2023-11-16 16:04:16 +0100

    redid parts  of TUI_Manager

--- src/main/java/CDIO3/TUI/TUI_Manager.java
+++ src/main/java/CDIO3/TUI/TUI_Manager.java
@@ -4,27 +4,25 @@ public class TUI_Manager {
   private final static String [] actions = new String[]{"se information about all players","roll for this turn"};
   private static BoardPrinter print = new BoardPrinter(null); 
   /**
-   * 
-   * @return true means to roll
-   * @return false  means to provide details and call this method again after
+   * reads a user input and shows the user the requested information to the user
+   * @return on roll command, when the  user rolls
    */
-  public static boolean readTurn(){
+  public static void readTurn(String details){
     while (true) {
       Commands command = UserInput.getCommand(actions,"please choose and action");
       switch (command) {
         case detail:
-          return false;
+          System.out.println("details");;
         case roll:
-          return true;
+          return;
         case inspect:
-          print.inspect(UserInput.getInt("which piece do you wish to  inspect: "));
+          print.inspect(UserInput.getInt("which piece do you wish to inspect: "));
           break;
         case guide:
           BoardPrinter.guide();
           break;
         case board:
           print.print();
-        default:
           break;
       }
     }
