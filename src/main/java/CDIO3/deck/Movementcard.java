c2fdc73bc2b397a9b29c7b2ec285effa30a2f32f
Asger <asger.piester@gmail.com>
2023-11-21 20:19:45 +0100

    added methods to move with the movementcard and also a method for when u draw the card

--- src/main/java/CDIO3/deck/Movementcard.java
+++ src/main/java/CDIO3/deck/Movementcard.java
@@ -1,7 +1,18 @@
 package CDIO3.deck;
 
+import CDIO3.Player.Player;
+
 public class Movementcard extends Card {
-    //TODO move og land on
+    private int MOVEMENT_VALUE;
 
     
+
+    public void move(Player currentPlayer) {
+        currentPlayer.getPiece().move(MOVEMENT_VALUE);
+    }
+
+    public void onDraw(Player currentPlayer) {
+        move(currentPlayer);
+    }
+
 }
