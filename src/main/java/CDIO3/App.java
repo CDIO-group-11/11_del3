8b93e092b28c41d288569250c63616b7b484f9c0
dendersen <dendersen@endersen.dk>
2023-11-21 12:49:48 +0100

    starting game logic

--- src/main/java/CDIO3/App.java
+++ src/main/java/CDIO3/App.java
@@ -1,15 +1,38 @@
 package CDIO3;
 
+import CDIO1.RaffleCup;
+import CDIO3.Player.Player;
+import CDIO3.TUI.TUI_Manager;
+import CDIO3.TUI.UserInput;
+import CDIO3.Tiles.Board;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+  private static Player[] players;
+  private static RaffleCup cup = new RaffleCup(2,6);
+  private static TUI_Manager TUI;
+  private static Board table;
+  private static int currentPlayer;
+  public static void main( String[] args ){
+    int playerCount = UserInput.getInt("how many players will be in the game? ",2,4);
+    players = new Player[playerCount];
+    for (int i = 0; i < playerCount ; i++) {
+      players[i] = new Player();
     }
+    table = new Board();
+    TUI = new TUI_Manager(table);
+    int die0 = -1, die1 = -1; 
+    while (true) {
+      TUI.readTurn(die0,die1,currentPlayer,players);
+      cup.roll();
+      die0 = cup.getSides()[0];
+      die1 = cup.getSides()[1];
+      players[currentPlayer].getPiece();
+    }
+  }
 
   public static String[] getOwnerList() {
     return null;
