7fa4a169d3aabd8fab40a9cf0cad568da798c11a
Asger <asger.piester@gmail.com>
2023-11-21 20:21:13 +0100

    changed the type

--- src/main/java/CDIO3/Player/Player.java
+++ src/main/java/CDIO3/Player/Player.java
@@ -3,13 +3,13 @@ package CDIO3.Player;
 public class Player {
     public static int Pamount;
     Wallet wallet = new Wallet();
-    int piece;
+    Piece piece;
     String propertyDeeds;
 
     public Wallet getWallet() {
         return wallet;
     }
-    public int getPiece() {
+    public Piece getPiece() {
         return piece;
     }
     public String getPropertyDeeds() {
