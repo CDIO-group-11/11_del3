30f91feec90a18cf0cf26cab036fbb52f99e2f5b
zling2200 <Mattias@bresdahl.dk>
2023-11-21 16:59:04 +0100

    Fixed Piece. Får nu dice værdi og ligger til spiller position

--- src/main/java/CDIO3/Player/Piece.java
+++ src/main/java/CDIO3/Player/Piece.java
@@ -1,21 +1,23 @@
 package CDIO3.Player;
 
-import CDIO1.RaffleCup;
-
 public class Piece {
     int piece = 0;
-    
-    //Get for dice værdi
-    public void getdice() {
-        RaffleCup raffleCup = new RaffleCup();
-        int diceValue = raffleCup.roll();
 
+    //Get for dice værdi
+    public Piece(int initialPosition){
+        this.piece = initialPosition;
+    }
+    public void updatePosition(int[] diceValues) {
+        // Sum the values of all dice rolls
+        int sum = 0;
+        for (int value : diceValues) {
+            sum += value;
+        }
+    }
     //Tilføjer dice værdi til nuværende position og sikre sig det ikke er over 24 (Støresle på board)
-    public void move(int diceValue){
-        piece += diceValue;
+    public void move(int sum){
+        piece += sum;
         piece %= 25;
     }
-
-    //
 
 }
