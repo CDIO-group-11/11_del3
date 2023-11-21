173a6b481e95e1ea29e0eb468f7227d0a0dcd204
Oscar Illerup <oillerup@gmail.com>
2023-11-21 20:54:53 +0100

    bug fix

--- src/main/java/CDIO3/Tiles/Purchaseable.java
+++ src/main/java/CDIO3/Tiles/Purchaseable.java
@@ -2,8 +2,8 @@ package CDIO3.Tiles;
 
 public class Purchaseable extends Tile {
 
-  public Purchaseable(Color color, String name, int tileNR) {
-    super(color, name, tileNR);
+  public Purchaseable(Color color, String name, int tileNR, int price, int ownedby) {
+    super(color, name, tileNR, price, ownedby);
     //TODO Auto-generated constructor stub
   }
     
