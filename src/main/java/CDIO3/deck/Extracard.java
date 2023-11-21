5a56774ca9b8fc574f822420ad1d701af081957a
Asger <asger.piester@gmail.com>
2023-11-21 20:25:21 +0100

    deleted 2 files. they were not used

--- src/main/java/CDIO3/deck/Extracard.java
+++
@@ -1,5 +0,0 @@
-package CDIO3.deck;
-
-public class Extracard extends Card {
-    
-}
--- src/main/java/CDIO3/deck/specificCards/Draw.java
+++
@@ -1,7 +0,0 @@
-package CDIO3.deck.specificCards;
-
-import CDIO3.deck.Extracard;
-
-public class Draw extends Extracard {
-    
-}
