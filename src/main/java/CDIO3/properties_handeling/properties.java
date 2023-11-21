22f62e74667df92c6a5d43b3d0d98bdc6ba136a4
Oscar Illerup <oillerup@gmail.com>
2023-11-21 12:25:31 +0100

    Name edit of file properties

---
+++ src/main/java/CDIO3/properties_handeling/properties.java
@@ -0,0 +1,17 @@
+class Property {
+    String name;
+    int price;
+
+    public Property(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
