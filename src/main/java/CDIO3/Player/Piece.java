5e080ef226511b6f90efbce685e1347f0d690dc5
Zling <Mattias@bresdahl.dk>
2023-11-21 11:22:42 +0100

    Updated Piece class til at get dice værdi og opdatere spilleresn position

--- src/main/java/CDIO3/Player/Piece.java
+++ src/main/java/CDIO3/Player/Piece.java
@@ -1,5 +1,21 @@
 package CDIO3.Player;
 
+import CDIO1.RaffleCup;
+
 public class Piece {
+    int piece = 0;
     
+    //Get for dice værdi
+    public void getdice() {
+        RaffleCup raffleCup = new RaffleCup();
+        int diceValue = raffleCup.roll();
+
+    //Tilføjer dice værdi til nuværende position og sikre sig det ikke er over 24 (Støresle på board)
+    public void move(int diceValue){
+        piece += diceValue;
+        piece %= 25;
+    }
+
+    //
+
 }
