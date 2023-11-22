2d1104cd987d60e1f134d0d8350be8e1d487f38c
dendersen <dendersen@endersen.dk>
2023-11-22 10:42:17 +0100

    fixed spelling

--- src/main/java/CDIO3/Tiles/differentCards/Beach.java
+++ src/main/java/CDIO3/Tiles/differentCards/Beach.java
@@ -4,9 +4,9 @@ package CDIO3.Tiles.differentCards;
 import CDIO3.Tiles.Color;
 import CDIO3.Tiles.Tile;
 
-public class Boardwalk extends Tile{
+public class Beach extends Tile{
 
-    public Boardwalk(int tileNR) {
+    public Beach(int tileNR) {
         super(Color.brown, "Beach", tileNR, 4, -1);
     }
 
