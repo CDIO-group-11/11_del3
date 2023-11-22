df12b72ab121eb9b1e1d9644090b02d9624e0366
dendersen <dendersen@endersen.dk>
2023-11-22 10:40:13 +0100

    fixed spelling mistake

--- src/main/java/CDIO3/Tiles/Tile.java
+++ src/main/java/CDIO3/Tiles/Tile.java
@@ -6,16 +6,15 @@ public abstract class Tile {
   protected String name;
   protected int tileNR;
   public int price;  
-  protected int ownedby;
+  protected int ownedBy;
 
 
-  public Tile(Color color, String name, int tileNR, int price, int ownedby){
+  public Tile(Color color, String name, int tileNR, int price, int ownedBy){
     this.color = color;
     this.name = name;
     this.tileNR = tileNR;
     this.price = price;
-    this.ownedby = ownedby;
-    
+    this.ownedBy = ownedBy;
   }
   public String getName() {
     return name;
@@ -30,12 +29,10 @@ public abstract class Tile {
     return price;
   }
   public int getOwnedBy() {
-    return ownedby;
-  }  
-  public int getOwnedBy() {
-    return ownedby;
+    return ownedBy;
   }  
   public void setOwnedBy(int newOwner) {
-    this.ownedby = newOwner;
+    this.ownedBy = newOwner;
   }
+  abstract public void landOn();
 }
