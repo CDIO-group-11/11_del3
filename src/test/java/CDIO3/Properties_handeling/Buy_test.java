b2fe61fe90e6f60fd7a96beec00a2a08cca5e4fb
Oscar Illerup <oillerup@gmail.com>
2023-11-23 19:01:16 +0100

    Get and pay-rent addet

---
+++ src/test/java/CDIO3/Properties_handeling/Buy_test.java
@@ -0,0 +1,21 @@
+package CDIO3.Properties_handeling;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import CDIO3.Tiles.Board;
+import CDIO3.Player.Player;
+import CDIO3.Tiles.Tile;
+import CDIO3.Tiles.differentCards.TestTile;
+
+
+public class Buy_test {
+    
+    @Test
+    public void Buy_test(){
+        Player player1 = new Player();
+        Tile tile = TestTile(1);
+        assertTrue( true );
+    }
+
+}
