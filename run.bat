6d2476a1f2b7c78794613ada581d3fc4edc94c21
dendersen <dendersen@endersen.dk>
2023-11-24 01:31:03 +0100

    added minmum terminal size to .bat

--- run.bat
+++ run.bat
@@ -1,7 +1,11 @@
 
 @echo off
-javac -sourcepath ./src/main/java/ -d ./bin/ -nowarn ./src/main/java/CDIO3/App.java
+call mvn verify
+javac -sourcepath ./src/main/java/ -d ./bin/ -encoding ISO-8859-1 -nowarn ./src/main/java/CDIO3/App.java
 echo if there are problems with wierd characters/overlapping text, you shoud use runClean.bat instead
+pause
+for /L %%G IN (0,1,50) DO echo ************************************************************
+echo make sure you can see the entire square at once, use ("ctrl" and "-") ro zoom out, ("ctrl" and "+") to zoom in
 pause
 java -cp ./bin/ CDIO3/App
 Pause
