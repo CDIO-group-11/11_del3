1b3f0aed53af63be06a222e3d0eb41b46bfef6c3
dendersen <dendersen@endersen.dk>
2023-11-24 13:29:07 +0100

    aditional board tests

--- src/test/java/CDIO3/Tiles/BoardTest.java
+++ src/test/java/CDIO3/Tiles/BoardTest.java
@@ -40,4 +40,37 @@ public class BoardTest {
     }
     assertArrayEquals(correct, t);
   }
+    public void correctTilesDan(){
+    String[] correct = new String[]{
+      "Start",
+      "Burgerbaren",
+      "Pizzeriaet",
+      "Chance",
+      "Slikbutikken",
+      "Iskiosken",
+      "På besøg i fængsel",
+      "Museet",
+      "bibliotek",
+      "Chance",
+      "Skaterparken",
+      "Vandlandet",
+      "Gratis parkering",
+      "Spillehallen",
+      "Biografen",
+      "Chance",
+      "Legetøjsbutikken",
+      "Dyrehandel",
+      "Gå i fængsel",
+      "Bowlinghallen",
+      "Zoo",
+      "Chance",
+      "Swimmingpoolen",
+      "Strandpromenaden"
+    };
+    String[] t = new String[24];
+    for (int i = 0; i < t.length; i++) {
+      t[i] = b.getTile(i).getName();
+    }
+    assertArrayEquals(correct, t);
+  }
 }
