002203e35dbb1247b42d83c8f832a0a1ab7f2ddf
dendersen <dendersen@endersen.dk>
2023-11-24 11:22:07 +0100

    added playercount into lang

--- src/main/java/CDIO3/lang/Lang.java
+++ src/main/java/CDIO3/lang/Lang.java
@@ -1,10 +1,29 @@
 package CDIO3.lang;
 
 public class Lang {
-  
-  private static String
-  e;
-  // public static getString(Object type, int callID){
+  private static Language lang = Language.en;
+  public static void setLang(){
 
-  // }
+  }
+  private static String[]
+  playerCount = new String[] {"how many players will be in the game? ","DK"};
+
+  public static String getSring(int ID){
+    switch (ID) {
+      case 0:
+        return playerCount[lang.ordinal()];
+      case 1:
+        return playerCount[lang.ordinal()];
+      case 2:
+        return playerCount[lang.ordinal()];
+      case 3:
+        return playerCount[lang.ordinal()];
+      case 4:
+        return playerCount[lang.ordinal()];
+      case 5:
+        return playerCount[lang.ordinal()];
+      default:
+        return null;
+    }
+  }
 }
