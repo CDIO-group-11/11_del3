a83f2abe0c4161fc2fd45a471dff01b7335e4ecf
dendersen <dendersen@endersen.dk>
2023-11-24 11:28:04 +0100

    arcade

--- src/main/java/CDIO3/Tiles/differentCards/Arcade.java
+++ src/main/java/CDIO3/Tiles/differentCards/Arcade.java
@@ -3,11 +3,12 @@ package CDIO3.Tiles.differentCards;
 
 import CDIO3.Tiles.Color;
 import CDIO3.Tiles.Tile;
+import CDIO3.lang.Lang;
 
 public class Arcade extends Tile{
 
     public Arcade(int tileNR) {
-        super(Color.brown, "Video game arcade", tileNR, 3, -1);
+        super(Color.brown, Lang.getSring(1), tileNR, 3, -1);
     }
 
     @Override
