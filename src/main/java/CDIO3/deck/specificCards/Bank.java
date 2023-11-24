0aad36edd8a6ede1a83afe312d66f7e15751c316
Asger <asger.piester@gmail.com>
2023-11-24 16:40:07 +0100

    made changes to the classes

--- src/main/java/CDIO3/deck/specificCards/Bank.java
+++ src/main/java/CDIO3/deck/specificCards/Bank.java
@@ -1,7 +1,13 @@
 package CDIO3.deck.specificCards;
 
-import CDIO3.deck.Moneycard;
+import CDIO3.deck.Card;
 
-public class Bank extends Moneycard {
+public class Bank extends Card {
     final int VALUE = 3;
+    final String name = "Bank";
+
+
+    public Bank() {
+        super("Bank", 3, 0, 0, "money");
+    }
 }
--- src/main/java/CDIO3/deck/specificCards/Jail.java
+++ src/main/java/CDIO3/deck/specificCards/Jail.java
@@ -1,7 +1,13 @@
 package CDIO3.deck.specificCards;
 
+import CDIO3.deck.Card;
 import CDIO3.deck.Movementcard;
 
-public class Jail extends Movementcard {
+public class Jail extends Card {
     final int TILE_VALUE = 18;
+    final String name = "Jail";
+
+    public Jail() {
+        super("Jail", 0, 0, 18, "move");
+    }
 }
